ddb4b6a3b5d39583ee83405ff0ba7d6cd328e945
Hien N <nguyehi.t@gmail.com>
2019-11-01 23:00:31 -0700

    Fix comment line

--- problems/two-city-scheduling.py
+++ problems/two-city-scheduling.py
@@ -2,10 +2,11 @@ def twoCitySchedCost(self, costs: List[List[int]]) -> int:
     # The cost differences tell us how much cost we can avoid by picking city A over B.        
     a = sorted(costs, key=lambda cost: cost[1] - cost[0], reverse=True)
     
-    #  Since our cutoff is the (N+1)th person, we send the remaining N people to city B.
+    # Send first N people to city A where the cost is minimized.
     res, n = 0, len(costs)//2
     for cost in a[:n]:
         res += cost[0]
+    #  Since our cutoff is the (N+1)th person, we send the remaining N people to city B.
     for cost in a[n:]:
         res += cost[1]
     return res
